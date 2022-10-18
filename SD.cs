be8cbe6eff448a12d12d8be0572af4d9b4aed945
Balraj <84559920+Ballu1811@users.noreply.github.com>
2022-10-18 17:30:24 +0530

    Add files via upload

---
+++ SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Park_Web_App
+{
+    public class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44314/";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/NationalPark/";
+        public static string TrailAPIPath = APIBaseUrl + "api/Trail/";
+    }
+}
